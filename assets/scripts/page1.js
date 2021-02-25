537bb5811d98e8fc8bac5e8512ef99a61a670f32
Usman-Kashmiri <75874034+Usman-Kashmiri@users.noreply.github.com>
2021-02-25 10:54:47 +0500

    Added Audio Player

---
+++ assets/scripts/page1.js
@@ -0,0 +1,113 @@
+// var h = '<audio controls src="./audio-test-01.mp3" autoplay="autoplay"></audio><audio controls src="https://mycugb.com/17ing/music/yc_yixin_yunkaiwusan.mp3"></audio><audio controls src="https://mycugb.com/17ing/music/17ing_20130508.mp3"></audio>'
+var h = '<audio controls src="./assets/audios/Salam Alaikum.mp3"></audio>'
+
+$(document).ready(function() {
+    $('.image-popup-no-margins').magnificPopup({
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+        image: {
+            verticalFit: false
+        },
+        zoom: {
+            enabled: true,
+            duration: 300
+        }
+    });
+    $('#app').html(h)
+
+    maudio({
+        obj: 'audio',
+        fastStep: 10
+    })
+
+    $('audio').on('abort', function() {
+        console.log('abort')
+    })
+
+    $('audio').on('canplay', function() {
+        console.log('canplay')
+    })
+
+    $('audio').on('canplaythrough', function() {
+        console.log('canplaythrough')
+    })
+
+    $('audio').on('durationchange', function() {
+        console.log('durationchange')
+    })
+
+    $('audio').on('emptied', function() {
+        console.log('emptied')
+    })
+
+    $('audio').on('ended', function() {
+        console.log('ended')
+    })
+
+    $('audio').on('error', function() {
+        console.log('error')
+    })
+
+    $('audio').on('loadeddata', function() {
+        console.log('loadeddata')
+    })
+
+    $('audio').on('loadedmetadata', function() {
+        console.log('loadedmetadata')
+    })
+
+    $('audio').on('loadstart', function() {
+        console.log('loadstart')
+    })
+
+    $('audio').on('pause', function() {
+        console.log('pause')
+    })
+
+    $('audio').on('play', function() {
+        console.log('play')
+    })
+
+    $('audio').on('playing', function() {
+        console.log('playing')
+    })
+
+    $('audio').on('progress', function() {
+        console.log('progress')
+    })
+
+    $('audio').on('ratechange', function() {
+        console.log('ratechange')
+    })
+
+    $('audio').on('seeked', function() {
+        console.log('seeked')
+    })
+
+    $('audio').on('seeking', function() {
+        console.log('seeking')
+    })
+
+    $('audio').on('stalled', function() {
+        console.log('stalled')
+    })
+
+    $('audio').on('suspend', function() {
+        console.log('suspend')
+    })
+
+    $('audio').on('timeupdate', function() {
+        console.log('timeupdate')
+    })
+
+    $('audio').on('volumechange', function() {
+        console.log('volumechange')
+    })
+
+    $('audio').on('waiting', function() {
+        console.log('waiting')
+    })
+});
