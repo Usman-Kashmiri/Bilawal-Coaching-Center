9bebe47b4a498beb667aa85f44dbb17a4176926e
Usman-Kashmiri <75874034+Usman-Kashmiri@users.noreply.github.com>
2021-03-26 11:00:51 +0500

    fixed img on mobile view

--- assets/scripts/book.js
+++ assets/scripts/book.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    var audioElement = document.createElement('audio');
-    audioElement.setAttribute('src', 'assets/audios/Bruh.mp3');
+  var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', 'assets/audios/tamal.mp3');
 
-    $('#audio').click(function() {
-        audioElement.play();
-    });
+  $('#audio').click(function() {
+      audioElement.play();
+  });
 });
